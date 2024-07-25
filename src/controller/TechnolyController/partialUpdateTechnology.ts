3d95881e35197922940a455ce4293bd6ae16854f
lucio-souza <desouzaleitelucioweslley@gmail.com>
2024-07-25 10:58:50 -0300

    adcionando a rota patch de technology

---
+++ src/controller/TechnolyController/partialUpdateTechnology.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import { Request,Response } from "express";
+import { checkExistsUserAccount } from "../../middlewares/checkExistsUserAccount";
+import { TechnologyBody } from "../../types/TechnologyBody";
+import { TechnologyParams } from "../../types/TechnologyParams";
+
+const prisma=new PrismaClient();
+
+export const partialUpdateTechnology=async(req:Request<TechnologyParams, TechnologyBody>,res:Response)=>{
+    const {id}=req.params;
+    try {
+        const technology=await prisma.technology.update({
+            where:{
+                id
+            },
+            data:{
+                studied:true
+            }
+        })
+        res.status(201).json(technology);
+    } catch (error) {
+        res.status(404).json({error:"Mensagem de erro"});
+    }
+}
--- src/routes/TechnologyRoutes.ts
+++ src/routes/TechnologyRoutes.ts
@@ -3,9 +3,11 @@ import { createTechnology } from '../controller/TechnolyController/createTechnol
 import { findTechnologiesByUsername } from '../controller/TechnolyController/findTechnologiesByUsername';
 import { checkExistsUserAccount } from '../middlewares/checkExistsUserAccount';
 import { updateTechnology } from '../controller/TechnolyController/updateTechnology';
+import { partialUpdateTechnology } from '../controller/TechnolyController/partialUpdateTechnology';
 
 export const technologyRouter=express.Router();
 
 technologyRouter.post('/',checkExistsUserAccount,createTechnology);
 technologyRouter.get('/',checkExistsUserAccount,findTechnologiesByUsername);
 technologyRouter.put('/:id',checkExistsUserAccount,updateTechnology);
+technologyRouter.patch('/:id/studied',checkExistsUserAccount,partialUpdateTechnology)
